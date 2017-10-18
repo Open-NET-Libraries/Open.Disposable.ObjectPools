b6b314ac6f334a5a97f61d8211a96efffee6f315
Oren (electricessence) <oren@electrified.net>
2017-10-17 23:21:31 -0700

    Added Recycler.

---
+++ source/Recycler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Open.Disposable
+{
+	/// <summary>
+	/// This class is provided as an asynchronous queue for recycling instead of using a recycle delegate with an object pool and calling GiveAsync() which could pile up unnecessarily.
+	/// So if recycling an object takes extra time, this might be a good way to toss objects away and not have to worry about the heavy cost as they will one by one be processed back into the target pool.
+	/// </summary>
+	public class Recycler<T> : DisposableBase
+		where T : class
+	{
+		IObjectPool<T> _target;
+		ActionBlock<T> _bin;
+
+		public Recycler(			
+			IObjectPool<T> target,
+			Action<T> recycleFunction,
+			ushort limit = ushort.MaxValue)
+		{
+			if(recycleFunction==null) throw new ArgumentNullException("recycleFunction");
+			_target = target ?? throw new ArgumentNullException("target");
+
+			_bin = new ActionBlock<T>(item =>
+			{
+				if (_target != null)
+				{
+					recycleFunction(item);
+					_target?.Give(item);
+				}
+			});
+
+			if (target is DisposableBase d)
+				d.BeforeDispose += Pool_BeforeDispose;
+		}
+
+		public Recycler(
+			ushort limit,
+			IObjectPool<T> pool,
+			Action<T> recycleFunction) : this(pool, recycleFunction, limit)
+		{
+
+		}
+
+		void Pool_BeforeDispose(object sender, EventArgs e)
+		{
+			Dispose();
+		}
+
+		public bool Recycle(T item)
+		{
+			return _bin?.Post(item) ?? false;
+		}
+
+		public Task Close()
+		{
+			var b = _bin;
+			if (b == null) return Task.CompletedTask;
+			b.Complete();
+			return b.Completion;
+		}
+
+		protected override void OnDispose(bool calledExplicitly)
+		{
+			_bin.Complete();
+			_bin = null;
+			_target = null;
+		}
+	}
+}
