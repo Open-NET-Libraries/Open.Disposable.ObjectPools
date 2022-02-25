58835c3a49a17b58c9dfec51cd79e9d7e95cb218
electricessence <5899455+electricessence@users.noreply.github.com>
2022-02-24 16:36:47 -0800

    No functional changes.

--- tests/ObjectPoolSmokeTests.cs
+++ tests/ObjectPoolSmokeTests.cs
@@ -1,38 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace Open.Disposable
+namespace Open.Disposable;
+
+[TestClass]
+public class ObjectPoolSmokeTests
 {
-	[TestClass]
-	public class ObjectPoolSmokeTests
+	class IdContainer
 	{
-		class IdContainer
-		{
-			public int ID;
-		}
+		public int ID;
+	}
 
-		[TestMethod]
-		public void OptimisticArrayObjectPool_FactoryTest()
-		{
-			var i = 0;
-			var pool = OptimisticArrayObjectPool.Create(() => new IdContainer { ID = ++i });
-			Assert.AreEqual(1, pool.Take().ID);
-		}
+	[TestMethod]
+	public void OptimisticArrayObjectPool_FactoryTest()
+	{
+		var i = 0;
+		var pool = OptimisticArrayObjectPool.Create(() => new IdContainer { ID = ++i });
+		Assert.AreEqual(1, pool.Take().ID);
+	}
 
-		[TestMethod]
-		public void ListPool_RecycleTest()
-		{
-			var pool = ListPool<int>.Shared;
-			var list = pool.Take();
-			list.Add(1);
-			pool.Give(list);
-			Assert.AreEqual(list, pool.Take());
-			Assert.AreEqual(0, list.Count);
+	[TestMethod]
+	public void ListPool_RecycleTest()
+	{
+		var pool = ListPool<int>.Shared;
+		var list = pool.Take();
+		list.Add(1);
+		pool.Give(list);
+		Assert.AreEqual(list, pool.Take());
+		Assert.AreEqual(0, list.Count);
 
 #pragma warning disable CS0618 // Type or member is obsolete
-			Assert.ThrowsException<NotSupportedException>(() => pool.Dispose());
+		Assert.ThrowsException<NotSupportedException>(() => pool.Dispose());
 #pragma warning restore CS0618 // Type or member is obsolete
-		}
-
 	}
 }
